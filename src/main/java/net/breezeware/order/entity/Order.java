2b627aa3d8483d235ce542726dfe69622e861f67
Seenu <chand2ram@gmail.com>
2023-09-26 14:51:17 +0530

    Project Completed

--- src/main/java/net/breezeware/order/entity/Order.java
+++ src/main/java/net/breezeware/order/entity/Order.java
@@ -1,20 +1,36 @@
 package net.breezeware.order.entity;
-public class Order {
-    private int id;
-    private int userId;
-    private double totalCost;
-    private String email;
-    private String phoneNumber;
-    private String orderLocation;
-    private String status;
-    private String orderOn;
-    private String deliveryOn;
 
+/**
+ * The `Order` class represents an order entity with various attributes such as
+ * ID, user ID, total cost, contact information, order location, status, and
+ * timestamps for order and delivery.
+ */
+public class Order {
+
+    // Fields (Attributes)
+    private int id;             // The unique identifier for the order.
+    private int userId;         // The ID of the user who placed the order.
+    private double totalCost;   // The total cost of the order.
+    private String email;       // The email address associated with the order.
+    private String phoneNumber; // The phone number associated with the order.
+    private String orderLocation; // The location where the order should be delivered.
+    private String status;      // The status of the order (e.g., pending, delivered).
+    private String orderOn;     // The timestamp when the order was placed.
+    private String deliveryOn;  // The timestamp when the order was delivered (if applicable).
+
+    // Constructors...
+
+    /**
+     * Constructs an order with just the ID and user ID.
+     */
     public Order(int id, int userId) {
         this.id = id;
         this.userId = userId;
     }
 
+    /**
+     * Constructs an order with ID, user ID, total cost, and status.
+     */
     public Order(int id, int userId, double totalCost, String status) {
         this.id = id;
         this.userId = userId;
@@ -22,7 +38,11 @@ public class Order {
         this.status = status;
     }
 
-    public Order(int id, int userId, double totalCost, String email, String phoneNumber, String orderLocation, String status, String orderOn, String deliveryOn) {
+    /**
+     * Constructs an order with detailed information including contact details and timestamps.
+     */
+    public Order(int id, int userId, double totalCost, String email, String phoneNumber,
+                 String orderLocation, String status, String orderOn, String deliveryOn) {
         this.id = id;
         this.userId = userId;
         this.totalCost = totalCost;
@@ -34,85 +54,130 @@ public class Order {
         this.deliveryOn = deliveryOn;
     }
 
-    public Order(String email, String phoneNumber, String orderLocation) {
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.orderLocation = orderLocation;
-    }
+    // Getter and Setter methods for the fields...
 
+    /**
+     * Gets the unique identifier for the order.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier for the order.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the ID of the user who placed the order.
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the ID of the user who placed the order.
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * Gets the total cost of the order.
+     */
     public double getTotalCost() {
         return totalCost;
     }
 
+    /**
+     * Sets the total cost of the order.
+     */
     public void setTotalCost(double totalCost) {
         this.totalCost = totalCost;
     }
 
-
+    /**
+     * Gets the email address associated with the order.
+     */
     public String getEmail() {
         return email;
     }
 
-
+    /**
+     * Sets the email address associated with the order.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the phone number associated with the order.
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Sets the phone number associated with the order.
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    /**
+     * Gets the location where the order should be delivered.
+     */
     public String getOrderLocation() {
         return orderLocation;
     }
 
-
+    /**
+     * Sets the location where the order should be delivered.
+     */
     public void setOrderLocation(String orderLocation) {
         this.orderLocation = orderLocation;
     }
 
-
+    /**
+     * Gets the status of the order.
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Sets the status of the order.
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Gets the timestamp when the order was placed.
+     */
     public String getOrderOn() {
         return orderOn;
     }
 
+    /**
+     * Sets the timestamp when the order was placed.
+     */
     public void setOrderOn(String orderOn) {
         this.orderOn = orderOn;
     }
 
+    /**
+     * Gets the timestamp when the order was delivered (if applicable).
+     */
     public String getDeliveryOn() {
         return deliveryOn;
     }
 
-
+    /**
+     * Sets the timestamp when the order was delivered (if applicable).
+     */
     public void setDeliveryOn(String deliveryOn) {
         this.deliveryOn = deliveryOn;
     }
--- src/main/java/net/breezeware/order/entity/OrderList.java
+++ src/main/java/net/breezeware/order/entity/OrderList.java
@@ -1,12 +1,27 @@
 package net.breezeware.order.entity;
 
+/**
+ * The `OrderList` class represents a list of items included in an order with
+ * details such as item ID, quantity, and cost.
+ */
 public class OrderList {
-    private int id;
-    private int orderId;
-    private int foodItemId;
-    private int quantity;
-    private double cost;
 
+    // Fields (Attributes)
+    private int id;          // The unique identifier for the order list item.
+    private int orderId;     // The ID of the order to which this item belongs.
+    private int foodItemId;  // The ID of the food item included in the order.
+    private int quantity;    // The quantity of the food item in the order.
+    private double cost;     // The cost of the food item.
+
+    /**
+     * Constructs an order list item with details such as order ID, food item ID,
+     * quantity, and cost.
+     *
+     * @param orderId    The ID of the order to which this item belongs.
+     * @param foodItemId The ID of the food item included in the order.
+     * @param quantity   The quantity of the food item in the order.
+     * @param cost       The cost of the food item.
+     */
     public OrderList(int orderId, int foodItemId, int quantity, double cost) {
         this.orderId = orderId;
         this.foodItemId = foodItemId;
@@ -14,44 +29,74 @@ public class OrderList {
         this.cost = cost;
     }
 
+    // Getter and Setter methods for the fields...
 
+    /**
+     * Gets the unique identifier for the order list item.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier for the order list item.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the ID of the order to which this item belongs.
+     */
     public int getOrderId() {
         return orderId;
     }
 
+    /**
+     * Sets the ID of the order to which this item belongs.
+     */
     public void setOrderId(int orderId) {
         this.orderId = orderId;
     }
 
+    /**
+     * Gets the ID of the food item included in the order.
+     */
     public int getFoodItemId() {
         return foodItemId;
     }
 
+    /**
+     * Sets the ID of the food item included in the order.
+     */
     public void setFoodItemId(int foodItemId) {
         this.foodItemId = foodItemId;
     }
 
+    /**
+     * Gets the quantity of the food item in the order.
+     */
     public int getQuantity() {
         return quantity;
     }
 
-
+    /**
+     * Sets the quantity of the food item in the order.
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * Gets the cost of the food item.
+     */
     public double getCost() {
         return cost;
     }
 
+    /**
+     * Sets the cost of the food item.
+     */
     public void setCost(double cost) {
         this.cost = cost;
     }
